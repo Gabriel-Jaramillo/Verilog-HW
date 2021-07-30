a705ff2b44f651f327cf350951a92c5162bc3530
Gabriel-Jaramillo <79036565+Gabriel-Jaramillo@users.noreply.github.com>
2021-07-29 19:48:31 -0700

    Create posCount.v

---
+++ posCount.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+
+// (1)
+
+module posCount(CLK, sum);
+    input CLK;
+    output [3:0]sum;
+    reg [3:0]sum = 4'b0000;
+    
+    always @(posedge CLK) 
+    begin
+        sum = sum + 1;
+    end
+endmodule
+
+module negCount(CLK, sum);
+    input CLK;
+    output [3:0]sum;
+    reg [3:0]sum = 4'b0000;
+    
+    always @(negedge CLK) begin
+        sum = sum +1;
+    end
+endmodule
